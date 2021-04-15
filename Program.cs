6def79f1e83c9ff5d3c658c78ce57022ab2a92b2
clevanter <amqlaluyan@iskolarngbayan.pup.edu.ph>
2021-04-15 16:21:32 +0800

    initial commit - Client Management System

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ClientManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Verimk: your number #1 client management software!\n");
+            Console.Write("Home ");
+            Console.Write("Inbox ");
+            Console.Write("Leads ");
+            Console.Write("Contacts ");
+            Console.Write("Accounts ");
+            Console.Write("Task ");
+            Console.WriteLine("Reports\n");
+
+
+            leadManagement Leads = new leadManagement();
+
+            Console.WriteLine("Please choose the tab you want to view according to its first letter. Just type a letter then your good to go!");
+            string userChoice = Console.ReadLine();
+
+
+            if (userChoice.Equals("H", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Home: Ongoing :)"); // will consist of feed, summary of contacts & interaction tracking, calendar
+            }
+            else if (userChoice.Equals("I", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Inbox: Ongoing :)"); // will consist of conversation
+            }
+            else if (userChoice.Equals("L",StringComparison.OrdinalIgnoreCase))
+            {
+                Leads.leadTab();
+                Console.WriteLine(Leads); // another class // consist of leads (prospects that you are contacting to see if there will be an opportunity to provide them your goods or services.)
+            }
+            else if (userChoice.Equals("C", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Contacts: Ongoing :)"); // will consist of contacts (person(s) with whom a communication is established to pursue a relationship or a business opportunity.)
+            }
+            else if (userChoice.Equals("A", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Accounts: Ongoing"); // will consist business to business relationships.
+            }
+            else if (userChoice.Equals("T", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Task: Ongoing"); // will consist a task list or to-do list.
+            }
+            else if (userChoice.Equals("R", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Reports: Ongoing"); // will consist of reports (sales and etc)
+            }
+            else
+            {
+                return;
+
+
+
+            }
+        }
+    }
+}
---
+++ leadManagement.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientManagementSystem
+{
+    class leadManagement
+    {
+       public void leadTab() {
+
+            Console.WriteLine("To Create a lead. Please Type C");
+            string userChoice = Console.ReadLine();
+
+            
+            if (userChoice.Equals("C", StringComparison.OrdinalIgnoreCase))
+            {
+               
+                Console.WriteLine("Create Lead\n\n");
+
+                Console.WriteLine("Lead Information\n");
+
+                Console.WriteLine("Company Name: ");
+                    string companyName = Console.ReadLine();
+
+                Console.WriteLine("First Name:");
+                    string firstName = Console.ReadLine();
+
+                Console.WriteLine("Last Name:");
+                    string lastName = Console.ReadLine();
+
+                Console.WriteLine("Title Or Position:");
+                    string titleOrPosition = Console.ReadLine();
+
+                Console.WriteLine("E-mail Address:");
+                    string emailAdd = Console.ReadLine();
+
+                Console.WriteLine("Phone Number");
+                    int phoneNum = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Mobile Number");
+                    string mobileNum = Console.ReadLine();
+
+                Console.WriteLine("Website Link:");
+                    string websiteLink = Console.ReadLine();
+
+                Console.WriteLine("Lead Source:");
+                    string leadSource = Console.ReadLine();
+
+                Console.WriteLine("Lead Status");
+                    string leadStatus = Console.ReadLine();
+
+                Console.WriteLine("Industry:");
+                    string Industry = Console.ReadLine();
+
+                Console.WriteLine("Number Of Employees:");
+                    int numOfEmployees = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Annual Revenue:");
+                    int annualRevenue = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Rating:");
+                    string rating = Console.ReadLine();
+
+                Console.WriteLine("Skype Id:");
+                string skypeId = Console.ReadLine();
+
+
+                Console.WriteLine("Company Name: " + companyName);
+                Console.WriteLine("First Name: " + firstName);
+                Console.WriteLine("Last Name: " + lastName);
+                Console.WriteLine("Title or Position: " + titleOrPosition);
+                Console.WriteLine("E-mail Address: " + emailAdd);
+                Console.WriteLine("Phone Number: " + phoneNum);
+                Console.WriteLine("Mobile Number:" + mobileNum);
+                Console.WriteLine("Website Link: " + websiteLink);
+                Console.WriteLine("Lead Source: " + leadSource);
+                Console.WriteLine("Lead Status: " + leadStatus);
+                Console.WriteLine("Industry: " + Industry);
+                Console.WriteLine("Number of Employees: " + numOfEmployees);
+                Console.WriteLine("Annual Revenue: " + annualRevenue);
+                Console.WriteLine("Rating: " + rating);
+                Console.WriteLine("Skype ID: " + skypeId);
+
+            }
+            else {
+                return;
+            }
+
+            
+        
+        }
+    }
+}
