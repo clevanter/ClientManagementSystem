19cefc81721e6ec6e5b5a8b10c8437d03684f7e9
clevanter <81273941+clevanter@users.noreply.github.com>
2021-06-08 21:46:32 +0800

    Responsibilities and Relationships activity

---
+++ CMS.UI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using CMS.BL;
+
+
+namespace CMS.UI
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            UserManagement ToLogin = new UserManagement();
+            Choices choices = new Choices();
+            Console.WriteLine("Verimk: Your #1 Client Management Software\n");
+            Console.WriteLine("Please LOG-IN\n");
+            ToLogin.username = "Clevanter325";
+            ToLogin.password = "12345678";
+            ToLogin.Admin();
+            choices.DisplayAction();
+            choices.TheChoice();
+               
+        }
+    }
+}
+
+
+
+
+
